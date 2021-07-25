2dfe41c9f564cb7430d8f1a5ab259c15e0fbb426
RuslanUchan <ruslanuchan97@gmail.com>
2021-07-25 20:06:07 +0700

    working fetch & dump user anime & manga data

--- list-scraper.py
+++ list-scraper.py
@@ -9,8 +9,10 @@ result:
 	"anime_id": 2810,
 """
 
+import csv
 import json
 import os
+import time
 
 import requests
 from tqdm import tqdm
@@ -51,10 +53,56 @@ if __name__ == '__main__':
 
 	# prep out file
 	with open(OUT_FILE, 'w') as f:
-		headers = 'username,anime_id,anime_title,score,status'
+		headers = 'username,type,id,title,score,status'
 		f.write(headers)
+		f.write('\n')
 
-	# loop over
-	for d in tqdm(data):
-		anime_link = ANIME_LINK.replace('USERNAME', d.username)
-		manga_link = MANGA_LINK.replace('USERNAME', d.username)
+	# loop over all users
+	for u in tqdm(user_offset):
+		dump = []  # lists of data dumped to file
+
+		# get data from anime
+		offset = 0
+		while offset < u['total_anime']:
+			# format link & fetch
+			anime_link = ANIME_LINK.replace('USERNAME', u['username']).replace('OFFSET', str(offset))
+			payload = fetch(anime_link)
+			
+			# store data
+			for data in payload:
+				dump.append((
+					u['username'],
+					'anime',
+					data['anime_id'],
+					data['anime_title'],
+					data['score'],
+					data['status']
+				))
+
+			# increase offset for next turn
+			offset += OFFSET
+			time.sleep(2)
+
+		# get data from manga
+		offset = 0
+		while offset < u['total_manga']:
+			manga_link = MANGA_LINK.replace('USERNAME', u['username']).replace('OFFSET', str(offset))
+			payload = fetch(manga_link)
+			
+			for data in payload:
+				dump.append((
+					u['username'],
+					'manga',
+					data['manga_id'],
+					data['manga_title'],
+					data['score'],
+					data['status']
+				))
+
+			offset += OFFSET
+			time.sleep(2)
+
+		# dump to file
+		with open(OUT_FILE, 'a') as f:
+			w = csv.writer(f, delimiter=',', quotechar='"')
+			w.writerows(dump)
